18493e85f6bf6e48cfd581247bd88fe6a6636cca
Giovane Melo <giovane@uniplaclages.edu.br>
2025-03-04 21:45:46 -0300

    Adicionado ex. anuncio juniplac

---
+++ exAula01Juniplac/scripts.js
@@ -0,0 +1,3 @@
+alert(
+  "Fala, galera! O XXVII JUNIPLAC tá chegando, e é hora de mostrar quem manda dentro e fora das quadras! Nosso galo já tá afiando as esporas 🐓💪, então bora representar o seu curso e garantir aquela resenha braba!"
+);
