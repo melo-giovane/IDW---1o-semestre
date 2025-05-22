537fdf264ec6065d2f4b71530aad36fd020643e5
Giovane <giovanec.melo@hotmail.com>
2025-05-22 01:21:16 -0300

    Atividade objetos completa

--- atividades-objetos/script.js
+++ atividades-objetos/script.js
@@ -1,24 +1,32 @@
-Document.addEventListener("DOMContentLoaded", () => {
-  num1 = document.getElementById("num1").value;
-  operacao = document.getElementById("operacao").value;
-  num2 = document.getElementById("num2").value;
-  resultado = "";
-/*   if (num1 === "" || operacao === "" || num2 === "")
+document.addEventListener("DOMContentLoaded", () => {
+  /*   if (num1 === "" || operacao === "" || num2 === "")
   {
-    alert("Preencha o numero 1, a operação se é adição ou subtração e depois o numero 2");
+  alert("Preencha o numero 1, a operação se é adição ou subtração e depois o numero 2");
   } */
-
-  document.addEventListener('click', (e) => {
-    if (e.target.tagname === 'BUTTON')
-    {
-      
+ 
+ document.addEventListener('click', (e) => {
+   if (e.target.tagName === 'BUTTON'){
+    num1 = document.getElementById("num1").value;
+    operacao = document.getElementById("operacao").value;
+    num2 = document.getElementById("num2").value;
+    resultado = 0;
+      if (operacao.toUpperCase() === 'ADIÇÃO' || operacao.toUpperCase() === 'ADICAO'){
+       resultado = adi(Number(num1), Number(num2));
+       document.getElementById('resultado').innerText = resultado;
+      }
+      else if (operacao.toUpperCase() === 'SUBTRAÇÃO' || operacao.toUpperCase() === 'SUBTRACAO'){
+           resultado = sub(Number(num1), Number(num2));
+           document.getElementById('resultado').innerText = resultado;
+          }
     }
   })
 
-  function adi(){
-    resultado = num1 + num2;
+  function adi(num1, num2){
+    return num1 + num2
+    
   }
-  function sub(){
-    resultado = num1 - num2;
+  function sub(num1, num2){
+    return num1 - num2;
+    
   }
 });
