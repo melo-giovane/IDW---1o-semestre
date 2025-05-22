4982e7c1723279b6cbf97a0dce7045474707a2d5
Giovane <giovanec.melo@hotmail.com>
2025-05-22 02:05:58 -0300

    exercicio arrays

---
+++ atividade-array/script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let frutas = ["Banana", "Maça", "Morango", "Uva"];
+  const botaoInserir = document.getElementById("botaoInserir");
+  const botaoRemover = document.getElementById("botaoRemover");
+
+  botaoInserir.addEventListener("click", adicionar);
+  botaoRemover.addEventListener("click", remover);
+
+  listarFrutas();
+
+  function listarFrutas() {
+    const lista = document.getElementById("frutas");
+    lista.innerHTML = "";
+
+    frutas.forEach((fruta) => {
+      const novaFruta = document.createElement("li");
+      novaFruta.textContent = fruta;
+      lista.appendChild(novaFruta);
+    });
+  }
+
+  function adicionar() {
+    const input = document.getElementById("novaFruta");
+    const novaFruta = input.value.trim();
+    if (novaFruta !== "") {
+      frutas.push(novaFruta);
+      listarFrutas();
+      input.value = "";
+    }
+  }
+
+  function remover() {
+    const input = document.getElementById("novaFruta");
+    const frutaParaRemover = input.value.trim();
+
+    if (frutaParaRemover !== "") {
+      frutas = frutas.filter(
+        (fruta) => fruta.toLowerCase() !== frutaParaRemover.toLowerCase()
+      );
+
+      listarFrutas();
+      input.value = "";
+    }
+  }
+});
